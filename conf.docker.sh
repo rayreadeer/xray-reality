c63ac29cbe7989943dacf4ac7da4e9fca0419127
rayreadeer <131382088+rayreadeer@users.noreply.github.com>
2023-04-20 23:59:13 +0330

    Update conf.docker.sh

--- conf.docker.sh
+++ conf.docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-json=$(curl -s https://raw.githubusercontent.com/sajjaddg/xray-reality/master/config.json)
+json=$(curl -s https://raw.githubusercontent.com/rayreadeer/xray-reality/master/config.json)
 
 keys=$(xray x25519)
 pk=$(echo "$keys" | awk '/Private key:/ {print $3}')
@@ -8,7 +8,7 @@ serverIp=$(curl -s ifconfig.me)
 uuid=$(xray uuid)
 shortId=$(openssl rand -hex 8)
 url="vless://$uuid@$serverIp:443?path=%2F&security=reality&encryption=none&pbk=$pub&fp=chrome&type=http&sni=yahoo.com&sid=$shortId#IRVLESS-REALITY-04"
-
+mkdir -p /usr/local/etc/xray/
 newJson=$(echo "$json" | jq \
     --arg pk "$pk" \
     --arg uuid "$uuid" \
